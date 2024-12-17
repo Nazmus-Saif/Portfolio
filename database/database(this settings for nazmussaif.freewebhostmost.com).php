065d3befdfd39a8f0120f1a5ec64841a4112c44f
Saif <124804475+Nazmus-Saif@users.noreply.github.com>
2024-12-17 20:27:47 +0600

    Delete database directory

--- database/database(this settings for nazmussaif.freewebhostmost.com).php
+++
@@ -1,6 +0,0 @@
-<?php
-  $conn = new mysqli("localhost","pgvpwfqh_Saif","MD26842662NZ362968S7724A3162I4412F","pgvpwfqh_Autobiography");
-  if($conn -> connect_error) {
-      die("Connection Failed: " . $conn -> connect_error);
-   }
-?>
